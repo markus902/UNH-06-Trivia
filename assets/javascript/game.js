a4eff0500243fc0de4d96ce00d61d213d15a6f75
Markus Markowich <markus902@gmx.de>
2019-07-23 21:16:55 -0400

    added preliminary question objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+question1 = {
+    question: "The beaver is the national emblem of which country?",
+    A: "Canada",
+    B: "Russia",
+    C: "Germany",
+    D: "USA",
+    correct: "A"
+}
+
+question2 = {
+    question: "How many players are there in a baseball team?",
+    A: "5",
+    B: "6",
+    C: "8",
+    D: "9",
+    correct: "D"
+    
+}
+
+question3 = {
+    question: "What kind of person shall not be honored on a US postal stamp, according to the US postal service and the Citizen’s Stamp Advisory Commitee?",
+    A: "An Actor",
+    B: "A living person",
+    C: "A President",
+    D: "Employees of the postal service",
+    correct: "B"
+}
